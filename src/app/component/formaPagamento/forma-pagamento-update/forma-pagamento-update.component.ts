d67937ebf5604ed67bf692eafeeced43ac749fd0
Lucas18n <lucascurso3347@gmail.com>
2025-05-08 15:49:37 -0300

    m8

--- src/app/component/formaPagamento/forma-pagamento-update/forma-pagamento-update.component.ts
+++ src/app/component/formaPagamento/forma-pagamento-update/forma-pagamento-update.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormaPagamento } from '../forma-pagamento.model';
+import { FormaPagamentoService } from '../forma-pagamento.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-forma-pagamento-update',
@@ -7,4 +10,27 @@ import { Component } from '@angular/core';
 })
 export class FormaPagamentoUpdateComponent {
 
+  formaPagamento!: FormaPagamento;
+
+  constructor(private formaPagamentoservice: FormaPagamentoService,
+    private router: Router,
+    private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id')
+    this.formaPagamentoservice.readById(id!).subscribe((formaPagamento: FormaPagamento) =>{
+      this.formaPagamento = formaPagamento
+    })
+  }
+
+  updateFormaPagamento(): void {
+    this.formaPagamentoservice.update(this.formaPagamento).subscribe(() => {
+      this.formaPagamentoservice.showMessage('Forma De Pagamento atualizada com sucesso!')
+      this.router.navigate(['/formaPagamento'])
+    })
+  }
+
+  cancel(): void {
+    this.router.navigate(['/formaPagamento'])
+  }
 }
